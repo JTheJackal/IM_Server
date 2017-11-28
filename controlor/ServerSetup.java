5286c4e67652c471a33e4a55685fdced2888fa30
Joshua Styles <styles@outlook.com>
2017-11-28 02:37:50 +0000

    JSON Changes
    Removed need for serialised User object. Server now accepts JSON for everything.

--- controlor/ServerSetup.java
+++ controlor/ServerSetup.java
@@ -28,22 +28,31 @@ public class ServerSetup {
 				Socket s = ss.accept();
 
 				ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
-				User u = (User) ois.readObject();
+				JSONObject u = (JSONObject) ois.readObject();
+				//User u = (User) ois.readObject();
 				//Message m = new Message();
 				JSONObject m = new JSONObject();
 				ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
 				
-				if (u.getPasswd().equals("123456")) {
+				//if (u.getPasswd().equals("123456")) {
+				if (u.get("passwd").toString().equals("123456")) {
 					//m.setMesType("1");
 					m.put("mesType", MessageType.message_succeed);
 					oos.writeObject(m);
 
 					ServerConClientThread scct = new ServerConClientThread(s);
+					
+					/*
 					ManageServerConClientThread.addClientThread(u.getUserId(), scct);
+					*/
+					
+					ManageServerConClientThread.addClientThread(u.get("userId").toString(), scct);
+
 
 					scct.start();
 
-					scct.notifyOther(u.getUserId());
+					//scct.notifyOther(u.getUserId());
+					scct.notifyOther((String) u.get("userId"));
 				} else {
 					//m.setMesType("2");
 					m.put("mesType", MessageType.message_login_fail);
